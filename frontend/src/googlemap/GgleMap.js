ffb8bf73d79af4dab1bc83e484519cd6e03dfd7f
bhagavan12 <jsbteja123@gmail.com>
2023-09-21 12:38:19 +0530

    googlemapformcommit

--- frontend/src/googlemap/GgleMap.js
+++ frontend/src/googlemap/GgleMap.js
@@ -26,11 +26,46 @@
 // }
 
 // export default GgleMap;
-import React from 'react';
+// import React from 'react';
 import './GgleMap.css'; // Import the CSS file
 
-function GgleMap() {
+// function GgleMap() {
+//   return (
+//     <div className="map-container">
+//       <iframe
+//         className="map-iframe"
+//         frameBorder="0"
+//         scrolling="no"
+//         marginHeight="0"
+//         marginWidth="0"
+//         src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=india+(GymLocator)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+//         title="Gym Locator Map"
+//       >
+//         <a href="https://www.maps.ie/population/">Find Population on Map</a>
+//       </iframe>
+//     </div>
+//   );
+// }
+
+// export default GgleMap;
+import React, { useState } from 'react';
+// import './googlemap/GgleMap.css'; // Import the CSS file with the correct path
+
+function MapComponent() {
+  // Define state variables to store booking details
+  const [location, setLocation] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here, you can send the booking details to your server or perform any desired action
+    console.log('Booking Details:', { location, date, time });
+  };
+
   return (
+    <>
     <div className="map-container">
       <iframe
         className="map-iframe"
@@ -43,8 +78,48 @@ function GgleMap() {
       >
         <a href="https://www.maps.ie/population/">Find Population on Map</a>
       </iframe>
-    </div>
+      </div>
+      <div className="booking-form">
+        <h2>Book a Gym</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="location">Location:</label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="date">Date:</label>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="time">Time:</label>
+            <input
+              type="time"
+              id="time"
+              name="time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Book Now</button>
+        </form>
+      </div>
+      </>
   );
 }
 
-export default GgleMap;
+export default MapComponent;
