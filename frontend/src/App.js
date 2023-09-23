e51fe99f1bd54dee78f44eeaeb34fd65700ae64c
Chavalam <varshi1188@gmail.com>
2023-09-23 10:54:04 +0530

    Ready commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import MapComponent from './googlemap/GgleMap';
 function App() {
   return (
     <div className="App">
-      <h2>sih2023
-      < MapComponent/>
-      </h2>
+      <h2>Hello</h2>
     </div>
   );
 }
